7c7addb582cd4929b42316a18dde7de4268cfd67
Taku Ogawa <ogawtaku@gmail.com>
2024-07-29 23:02:42 +0900

    Add info

--- app/src/main/java/com/example/introductionapp/MainActivity.kt
+++ app/src/main/java/com/example/introductionapp/MainActivity.kt
@@ -12,17 +12,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.introductionapp.ui.theme.IntroductionAppTheme
@@ -36,12 +33,13 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     Column(
                         modifier = Modifier.padding(32.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,) {
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        ){
                         Image(painter = painterResource(id = R.drawable.image_profile),
                             contentDescription = "Profile Image",
                             modifier = Modifier
-                                .size(100.dp)
-                                .clip(RoundedCornerShape(10.dp)),
+                                .size(150.dp)
+                                .clip(RoundedCornerShape(20.dp)),
                             )
                         Spacer(modifier = Modifier.height(20.dp))
                         Text(text = "Taku Ogawa",
@@ -55,6 +53,24 @@ class MainActivity : ComponentActivity() {
                             fontSize = 16.sp,
                             fontWeight = FontWeight.ExtraBold,
                         )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Column(
+                            horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text(
+                                text = "フリーランス",
+                                fontSize = 26.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "テクノロジーグループ",
+                                color = Color.Gray,
+                                fontSize = 16.sp,
+                            )
+                        }
                     }
                 }
             }
