fbb13b5d1ecb4144a803bc43ee2ec96bb97a23a6
Taku Ogawa <ogawtaku@gmail.com>
2024-07-29 22:54:59 +0900

    Adjust layout

--- app/src/main/java/com/example/introductionapp/MainActivity.kt
+++ app/src/main/java/com/example/introductionapp/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -32,10 +35,15 @@ class MainActivity : ComponentActivity() {
             IntroductionAppTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     Column(
-                        modifier = Modifier.padding(top = 32.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
+                        modifier = Modifier.padding(32.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,) {
                         Image(painter = painterResource(id = R.drawable.image_profile),
-                            contentDescription = "Profile Image",)
+                            contentDescription = "Profile Image",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp)),
+                            )
+                        Spacer(modifier = Modifier.height(20.dp))
                         Text(text = "Taku Ogawa",
                             color = Color.Gray,
                             fontSize = 16.sp,
